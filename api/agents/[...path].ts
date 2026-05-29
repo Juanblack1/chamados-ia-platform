08a230d25581df342ed2792a0e2b531ca37d3370
Juan Veronez <veronezjuan@gmail.com>
2026-05-28 22:03:49 -0300

    add vercel api catchall routes

---
+++ api/agents/[...path].ts
@@ -0,0 +1,11 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { buildServer } from "../../backend/src/http/server.js";
+import { loadEnv } from "../../backend/src/config/env.js";
+
+let serverPromise: ReturnType<typeof buildServer> | undefined;
+
+export default async function handler(request: IncomingMessage, response: ServerResponse) {
+  const server = await (serverPromise ??= buildServer(loadEnv()));
+  await server.ready();
+  server.server.emit("request", request, response);
+}
