152e39ed2bd9a1baf0a594e793cc29a0b5ff3a16
Juan Veronez <veronezjuan@gmail.com>
2026-05-29 03:16:09 -0300

    use vercel api splat route

--- api/[...].ts
+++ api/[...].ts
@@ -4,7 +4,20 @@ import { buildServer } from "../backend/src/http/server.js";
 
 let serverPromise: ReturnType<typeof buildServer> | undefined;
 
+function preserveOriginalApiPath(request: IncomingMessage) {
+  if (!request.url) return;
+
+  const url = new URL(request.url, `https://${request.headers.host ?? "localhost"}`);
+  const rewrittenPath = url.searchParams.get("path");
+  if (!rewrittenPath) return;
+
+  url.pathname = `/api/${rewrittenPath}`;
+  url.searchParams.delete("path");
+  request.url = `${url.pathname}${url.search}`;
+}
+
 export default async function handler(request: IncomingMessage, response: ServerResponse) {
+  preserveOriginalApiPath(request);
   const server = await (serverPromise ??= buildServer(loadEnv()));
   await server.ready();
   server.server.emit("request", request, response);
